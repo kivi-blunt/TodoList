abd3e11df724dc2e741bf5f498ad926aac233701
kivi-blunt <vladik.zakaz@gmail.com>
2021-11-10 17:48:20 +0300

    Updates

---
+++ precache-manifest.061b54569e26b0daaaafe0d229d5051a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27797b5b72f3b0141b3ff810ba8af30",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "ab1c5b3533756df819d3",
+    "url": "/TodoList/static/css/main.f119ce6c.chunk.css"
+  },
+  {
+    "revision": "0de1a9896d6a01bb4282",
+    "url": "/TodoList/static/js/2.6d1dc2f0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.6d1dc2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1c5b3533756df819d3",
+    "url": "/TodoList/static/js/main.4a5492a8.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
